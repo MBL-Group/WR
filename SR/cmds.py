44e46153acf159a4255a8d4b8ae76b2a98ea88e6
universea <287827688@qq.com>
2024-08-13 15:58:12 -0400

    update nn model weights links

--- SR/cmds.py
+++ SR/cmds.py
@@ -15,7 +15,7 @@ python mmrealsr/train.py -opt options/finetune_MMRealSRNet_x4.yml --auto_resume
 python mmrealsr/train.py -opt options/finetune_MMRealSRGAN_x4.yml --auto_resume
 
 
-python inference_mmrealsrgpu1.py --im_path=/xxx-Data/MM-RealSR-Frelu/datasets/mutant_embryo_JAC624/Mutant-worm-50um_624_YA --res_path=/Pengsong-Data/Mutant-worm-50um_624_YA_SR/
-python inference_mmrealsrgpu2.py --im_path=/xxx-Data/MM-RealSR-Frelu/datasets/mutant_embryo_JAC634/Mutant-worm-50um_634_YA --res_path=/Pengsong-Data/Mutant-worm-50um_634_YA_SR/
-python inference_mmrealsrgpu3.py --im_path=/xxx-Data/MM-RealSR-Frelu/datasets/mutant_embryo_JAC984/Mutant-worm-50um_984_YA --res_path=/Pengsong-Data/Mutant-worm-50um_984_YA_SR/
+python inference_mmrealsrgpu1.py --im_path=/xxx-Data/MM-RealSR-Frelu/datasets/mutant_embryo_JAC624/Mutant-worm-50um_624_YA --res_path=/xxx-Data/Mutant-worm-50um_624_YA_SR/
+python inference_mmrealsrgpu2.py --im_path=/xxx-Data/MM-RealSR-Frelu/datasets/mutant_embryo_JAC634/Mutant-worm-50um_634_YA --res_path=/xxx-Data/Mutant-worm-50um_634_YA_SR/
+python inference_mmrealsrgpu3.py --im_path=/xxx-Data/MM-RealSR-Frelu/datasets/mutant_embryo_JAC984/Mutant-worm-50um_984_YA --res_path=/xxx-Data/Mutant-worm-50um_984_YA_SR/
 
